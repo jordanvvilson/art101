2abe29caf700d28952ca48663e68179ed3351390
jordanvvilson <130079900+jordanvvilson@users.noreply.github.com>
2023-06-01 00:35:29 -0700

    update

---
+++ lab15/js/lab15.js
@@ -0,0 +1,37 @@
+//Author: Jordan Wilon + Siena Dudas
+//Date 31 May 2023
+//Lab 15: AJAX
+//Credit to alvarotrigo.com, kanye.rest api, and chatgpt for helping me find my errors
+
+$(document).ready(function() {
+  // Add click event to the button
+  $('#activate').click(function() {
+    fetchQuote();
+  });
+});
+
+// Fetch and display a Kanye West quote
+function fetchQuote() {
+  $.ajax({
+     // The URL for the request (from the api docs)
+    url: "https://api.kanye.rest/",
+    //get request
+    method: "GET",
+    dataType: 'json',
+    success: function(response) {
+      var quote = response.quote;
+      displayQuote(quote);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown);
+    }
+  })
+}
+
+// Function to display the quote in the output div
+function displayQuote(quote) {
+  $("#output").text(quote);
+}
+
+
+
